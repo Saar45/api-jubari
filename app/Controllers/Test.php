e07d0887e96f37bf368c4148487af52c9ed235aa
Saar45 <nabysarr16@gmail.com>
2024-11-20 02:08:22 +0100

    jeu d'essai sur les modeles

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -3,14 +3,28 @@
 namespace App\Controllers;
 
 use App\Models\Employe;
+use App\Models\Congeindispo;
+use App\Models\Message;
 
 
 class Test extends BaseController
 {
     public function getIndex()
     {
-        $unemploye = Employe::find(1);
+        $unemploye = Employe::find(20);
         
-        echo "Nom de lemploye numero one : " . $unemploye->nomEmploye;
+        echo "Nom de l'employe number 4 : " . $unemploye->id ;
+
+        echo "<br>";
+
+        $unconge = Congeindispo::find(101);
+
+        echo "Conge 101: " . $unconge->description ;
+
+        echo "<br>";
+
+        $unmessage = Message::find(3);
+
+        echo "Message 3: " . $unmessage->description ;
     }
 }
